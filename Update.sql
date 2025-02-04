f63fb20e50c2c3aade2f35deae1cc831b6aa1afb
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-02-04 14:23:22 +0530

    Create Update.sql

---
+++ Update.sql
@@ -0,0 +1,14 @@
+update EMPLOYEE
+set name='pravin' 
+where empid=1;
+
+
+
+
+'1','pravin','Sales','69'
+'2','Dave','Accounting','86'
+'3','Ava','Sales','81'
+'4','DEV','Sales','98'
+'5','OMKAR','Accounting','78'
+'6','vinod','Sales','55'
+'7','pritesh','Sales','59'
