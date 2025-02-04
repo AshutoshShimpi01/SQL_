04328479574b2e666139d7b568e81d8103b71b9d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-02-04 17:43:42 +0530

    IN.sql

--- IN.sql
+++ IN.sql
@@ -8,3 +8,10 @@ customer_id	first_name	last_name	age	country
 2	Robert	Luna	22	USA
 3	David	Robinson	22	UK
 4	John	Reinhardt	25	UK
+
+
+
+Return all customers that have NOT placed any orders in the Orders table:
+
+SELECT * FROM Customers
+WHERE CustomerID NOT IN (SELECT CustomerID FROM Orders);
