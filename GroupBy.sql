b8d4be9afdd6a17d0597ed74d91ceaa4fba28eb1
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-02-04 18:02:50 +0530

    GroupBy.sql

--- GroupBy.sql
+++ GroupBy.sql
@@ -5,3 +5,13 @@ count the total sum of salary in each city pune,mumbai,delhi
 select sum(salary),city -- adding salary from pune,then mumbai,then delhi
 from employee
 group by city;     ---- grouping city first
+
+
+
+
+count the total number of employess which presents in each department
+
+
+select count(*),department
+from employee
+group by department;
