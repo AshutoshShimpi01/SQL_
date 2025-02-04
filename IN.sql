e488269e8b6f7e988a6db2ad40a6db1c05ab45ae
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-02-04 17:42:48 +0530

    Create IN.sql

---
+++ IN.sql
@@ -0,0 +1,10 @@
+select *
+from Customers
+where country in ('USA','UK');
+
+
+customer_id	first_name	last_name	age	country
+1	John	Doe	31	USA
+2	Robert	Luna	22	USA
+3	David	Robinson	22	UK
+4	John	Reinhardt	25	UK
