db7b809790c50757f5348aed5c90711af5f952f3
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-02-10 20:52:37 +0530

    SqlQueries.sql

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -6,5 +6,16 @@ select name,salary from EMPLOYEE
 where salary > 
      (select avg(salary) from employee);
 
+-------------------
+
+Display similar employess dept and theri names in sequential order
+
+select distinct(name),dept
+from employee 
+order by dept;
+
+-------------------
+
+
 
 
