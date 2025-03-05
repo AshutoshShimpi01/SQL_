20e4f6198437e1053cfbd250892de61f2b887b53
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-03-05 13:27:37 +0530

    CASEOrderby.sql

---
+++ CASEOrderby.sql
@@ -0,0 +1,18 @@
+SELECT name, age
+FROM student
+ORDER BY
+(CASE
+    WHEN name IS NULL THEN age
+    ELSE name
+END);
+
+'abhi_S','31'
+'ashutosh','23'
+'nikita','21'
+'omkar','27'
+'pawan','17'
+'riya','19'
+'rushi','21'
+'sai','18'
+'shruti','21'
+'vinod','25'
