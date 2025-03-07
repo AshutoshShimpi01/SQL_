10c9be89ac782bfd59135c7a1611169c3f8d7e5f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-03-07 11:49:44 +0530

    SQLFUNCTIONs.sql

---
+++ SQLFUNCTIONs.sql
@@ -0,0 +1,25 @@
+
+-find how many times a char repeated in name
+select length(name) -length(replace(name,'a','')) from student;
+
+
+- how many times as repeated in names
+select (length(name) -length(replace(name,'as',''))) / length('as') from student;
+
+
+-sh
+select (length(name) -length(replace(name,'sh',''))) / length('sh') from student;
+
+
+-find how many times a char repeated in sentence
+select length('This is a beautiful day, sql is easy')
+-length(replace('This is a beautiful day, sql is easy','a',''));
+
+
+-count Space in sentence
+select length('This is a beautiful day, sql is easy')
+-length(replace('This is a beautiful day, sql is easy',' ',''));
+
+
+
+
