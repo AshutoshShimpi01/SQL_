fdf6b5315c56a29440605a349b4e955fb50a48e3
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-03-07 18:22:04 +0530

    SubQuery.sql

--- SubQuery.sql
+++ SubQuery.sql
@@ -1,4 +1,13 @@
-The result in the example above returned 74 records, that means that there are 17 customers that haven't placed any orders.
+-The result in the example above returned 74 records, that means that there are 17 customers that haven't placed any orders.
 
 SELECT * FROM Customers
 WHERE CustomerID NOT IN (SELECT CustomerID FROM Orders);
+
+
+
+-find those customers_name whhose Order keyboard or mouse
+  
+select first_name,Country from Customers
+where customer_id in
+(select  customer_id from Orders 
+where item in('Keyboard','Mouse'));
