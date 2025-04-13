b37db74d594e7cecd9e7d7e8cb8b502ec55b575c
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-13 10:32:12 +0530

    JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -2,3 +2,12 @@ find all student detais and their dept_names from dept table , starting from max
 
 select * from (select *, row_number() over(partition by dept_id) r from student s
 where dept_id in (select dept_id,department_name from department) ;
+------
+select avg(marks),d.dept_name,s.student_id,s.gender
+from STUDENT s
+join department d
+on s.dept_id = d.dept_id
+group by d.dept_name,s.student_id,s.gender
+having avg(marks) > 80
+order by s.gender
+;
