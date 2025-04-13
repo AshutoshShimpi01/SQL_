69805fec68d0c21cf336e819266c3f24ed5e7113
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-13 10:56:54 +0530

    JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -11,3 +11,12 @@ group by d.dept_name,s.student_id,s.gender
 having avg(marks) > 80
 order by s.gender
 ;
+
+
+Count of Students Per Department Who Scored More Than 80 Marks
+
+SELECT d.dept_name, COUNT(s.student_id) AS high_scorers
+FROM student s
+JOIN department d ON s.dept_id = d.dept_id
+WHERE s.marks > 80
+GROUP BY d.dept_name;
