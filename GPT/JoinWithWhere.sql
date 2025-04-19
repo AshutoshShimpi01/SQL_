238970fc867b13734f57ed88b35f00623cd876f4
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-19 11:22:55 +0530

    JoinWithWhere.sql

--- GPT/JoinWithWhere.sql
+++ GPT/JoinWithWhere.sql
@@ -8,3 +8,13 @@ on e.emp_id = p.emp_id
 where salary between 5000 and 6000
 group by p.emp_id
 having count(p.emp_id) > 1;
+
+
+---—Find employee names who are not assigned to any project and belong to departments other than ‘Marketing’.;
+
+select e.name
+from employees e
+left join projects p
+on e.emp_id =p.emp_id
+join departments d on e.dept_id = d.dept_id
+where project_id is null and dept_name <> 'Marketing';
