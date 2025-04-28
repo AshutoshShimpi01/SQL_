d7b7b17ec3853a5f63b95bd09bf59d05d665e3af
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-28 23:00:51 +0530

    JoinWhere.sql

--- COPILOT/JoinWhere.sql
+++ COPILOT/JoinWhere.sql
@@ -4,3 +4,14 @@ SELECT e.*
 FROM employees e
 LEFT JOIN employee_project_assignments ea ON e.emp_id = ea.emp_id
 WHERE ea.project_id IS NULL;
+
+--Find the employee(s) with the highest salary in each department.;
+
+select e.name,e.salary 
+from employees e
+join departments d on e.dept_id = d.dept_id
+where (d.dept_name,e.salary) in
+(select d.dept_name,max(e.salary) as highest_sal
+from employees e
+join departments d on e.dept_id = d.dept_id
+group by d.dept_name);
