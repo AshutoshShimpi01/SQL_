59f8be42baf92c6e2f3ce58dfc8c64207ea8a719
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-29 11:30:41 +0530

    WindowFun.sql

--- WindowFun.sql
+++ WindowFun.sql
@@ -49,4 +49,34 @@ find all emp details which got maximum marks in their dept.
 select *, row_number() over(partition by dept order by marks desc) as dept from employee ;
 
 
+NTILE()
+
+--divide employees into 3 salary buckets and labeling them
+
+select emp_id,
+      case when x.bucket = 1 then 'high salary'
+           when x.bucket = 2 then 'midium salary'
+           when x.bucket = 3 then 'low salary'
+      end as salary_range
+from (select *,
+ntile(3) over(order by salary desc) as bucket
+from employees) x;
+
+--count how many employees fall into each salary range using your NTILE-based buckets
+
+SELECT 
+    CASE 
+        WHEN bucket = 1 THEN 'high salary'
+        WHEN bucket = 2 THEN 'medium salary'
+        WHEN bucket = 3 THEN 'low salary'
+    END AS salary_range,
+    COUNT(*) AS num_employees
+FROM (
+    SELECT NTILE(3) OVER (ORDER BY salary DESC) AS bucket
+    FROM employees
+) ranked
+GROUP BY bucket
+ORDER BY bucket;
+
+
 
