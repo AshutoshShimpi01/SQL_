997e2e1b575a3b01cd701dc3623602cd6f6b8d8b
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-04-29 10:54:06 +0530

    WindowFun.sql

--- WindowFun.sql
+++ WindowFun.sql
@@ -1,3 +1,5 @@
+RANK()
+
 select e.*, rank() over(order by marks desc) as rnk from employee e;
 '4','DEV','Sales','98','Ganesh','1'
 '3','Ava','Sales','90','Ganesh','2'
@@ -7,6 +9,17 @@ select e.*, rank() over(order by marks desc) as rnk from employee e;
 '1','Clark','Sales','69','Ganesh','6'
 '7','pritesh','Sales','59','Ganesh','7'
 '6','vinod','Sales','55','Ganesh','8'
+
+--it fetchs only 1st rank salary in each dept
+WITH ranked_employees AS (
+    SELECT e.*, 
+           RANK() OVER (PARTITION BY dept_id ORDER BY salary DESC) AS rak
+    FROM employees e
+)
+SELECT *
+FROM ranked_employees
+WHERE rak = 1;
+
 
 Dense_rank()
 
