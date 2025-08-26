15ce7a4d12b96bb28cae96398b9b5296a1f037d3
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-08-26 10:50:17 +0530

    Update WindowFunc-for-Interv.sql

--- COPILOT/WindowFunc-for-Interv.sql
+++ COPILOT/WindowFunc-for-Interv.sql
@@ -8,3 +8,18 @@ from employees e
 join departments d
 on e.dept_id = d.dept_id) as x
 where x.rk <= 2;
+
+
+-- Top 2 highest-paid employees per department
+
+
+SET @rank := 0;
+SET @prev_region := '';
+
+select * from
+(select e.emp_id, e.name, d.dept_name, e.salary,
+rank() over(partition by dept_name order by salary desc) rk
+from employees e
+join departments d
+on e.dept_id = d.dept_id) x
+where x.rk < 3;
