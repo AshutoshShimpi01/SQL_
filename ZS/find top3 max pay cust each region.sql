3157f8e356143a6f65e03dd1dfb06d314946008f
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-09-01 11:56:24 +0530

    Update find top3 max pay cust each region.sql

--- ZS/find top3 max pay cust each region.sql
+++ ZS/find top3 max pay cust each region.sql
@@ -25,3 +25,30 @@ SELECT *                            -- (3) Keep only the Top 3 ranks (with ties)
 FROM ranked
 WHERE rk <= 3
 ORDER BY region, rk, total_spent DESC;  -- (4) Nice, readable output
+
+
+
+-- written by me
+
+-- Top 3 customers by total spending in year 2024 for each region
+
+
+with cust as
+(
+select c.cust_name,c.region,
+sum(total_amount) as total
+from customers c
+join Orders o on c.cust_id = o.cust_id
+where order_date between '2024-01-01' and '2024-12-31'
+group by c.cust_name,c.region
+),
+rak as
+(
+select c.cust_name,c.region,total,
+rank() over(partition by c.region order by total desc) as rk
+from cust
+)
+select *
+from rak
+where rk <= 3
+order by region, rk, total DESC;
