94c3d460798273d6a82658c3fed1b96af2ffc01e
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-09-21 09:42:18 +0530

    Update CTE.sql

--- COPILOT/CTE.sql
+++ COPILOT/CTE.sql
@@ -10,3 +10,19 @@ where e.salary > a.av;
 select * from employees 
 where salary >
 (select avg(salary) from employees);
+
+
+-- find employees whose salary is greater than their department’s average.
+-- SELF WRITTEN
+with dpt_avg as 
+(
+select d.dept_id,d.dept_name,avg(e.salary) as av
+from departments d
+join employees e on d.dept_id = e.dept_id
+group by d.dept_id,d.dept_name
+)
+select e.name
+from employees e
+join dpt_avg da on e.dept_id = da.dept_id
+where e.salary > da.av 
+;
