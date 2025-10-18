def63c6f2d9665b362581b08056347974efd0507
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-10-18 19:46:34 +0530

    Update List employees who worked on a same project from a different department.py

--- My-Solve-CODES/List employees who worked on a same project from a different department.py
+++ My-Solve-CODES/List employees who worked on a same project from a different department.py
@@ -1,6 +1,24 @@
 -- List employees who worked on a same project from a different department.
 
 
+simple-
+
+
+select distinct e1.name
+from employee_project_assignments ep1
+join employee_project_assignments ep2
+on ep1.project_id = ep2.project_id
+join employees e1
+on e1.emp_id = ep1.emp_id
+join employees e2
+on ep2.emp_id = e2.emp_id
+where e1.emp_id != e2.emp_id
+and e1.dept_id != e2.dept_id;
+
+
+
+with WindowFunctions
+
 
 
 WITH ProjectDeptInfo AS (
