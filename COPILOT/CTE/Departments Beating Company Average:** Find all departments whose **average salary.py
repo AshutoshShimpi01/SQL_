fd1ace7e82e184297e4fc7977a38ed1d3b0c95e4
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-10-21 14:10:12 +0530

    Update Departments Beating Company Average:** Find all departments whose **average salary.py

--- COPILOT/CTE/Departments Beating Company Average:** Find all departments whose **average salary.py
+++ COPILOT/CTE/Departments Beating Company Average:** Find all departments whose **average salary.py
@@ -3,6 +3,7 @@
 -- 6.  **Departments Beating Company Average:** Find all departments whose **average salary** is greater than the 
 -- **overall average salary** of all employees in the company. List the department name and its average salary.;
 
+
 with dpt as
 (
 select d.dept_name,
@@ -16,3 +17,24 @@ select dept_name, dept_av
 from dpt
 cross join overAll_av 
 where dept_av > av;
+
+
+
+SAME
+----
+
+
+with av as(
+select d.dept_name,avg(e.salary) as dpt_avg
+from employees e
+join departments d on e.dept_id = d.dept_id
+group by d.dept_name), total as
+(
+select avg(salary) as total_avg
+from employees
+)
+select dept_name,dpt_avg 
+from av
+cross join total
+where av.dpt_avg > total_avg;
+
