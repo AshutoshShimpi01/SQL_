e8b0a412f848b868871aebefeaa510f4322cfdab
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-10-27 11:01:25 +0530

    Update Employees Above Department Average.py

--- COPILOT/CTE/Employees Above Department Average.py
+++ COPILOT/CTE/Employees Above Department Average.py
@@ -13,3 +13,30 @@ from employees e
 )
 select * from av
 where salary > dept_av_sal;
+
+
+
+
+
+
+
+
+
+
+
+100 % solve by me without 1% of help
+
+
+with emp as
+(select e.dept_id,e.name,e.salary
+from employees e),
+dept_av as
+(select e.dept_id,avg(e.salary) as avv
+from employees e
+join departments d
+on e.dept_id = d.dept_id
+group by e.dept_id)
+select name,salary
+from emp
+join dept_av on emp.dept_id = dept_av.dept_id
+where emp.salary > dept_av.avv;
