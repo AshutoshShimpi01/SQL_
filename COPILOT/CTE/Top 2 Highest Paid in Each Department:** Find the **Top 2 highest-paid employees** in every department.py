2db3e3afbb302d3152e01fc369e8d9891e32491e
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-11-02 11:26:57 +0530

    Update Top 2 Highest Paid in Each Department:** Find the **Top 2 highest-paid employees** in every department.py

--- COPILOT/CTE/Top 2 Highest Paid in Each Department:** Find the **Top 2 highest-paid employees** in every department.py
+++ COPILOT/CTE/Top 2 Highest Paid in Each Department:** Find the **Top 2 highest-paid employees** in every department.py
@@ -11,3 +11,18 @@ join departments d on e.dept_id = d.dept_id
 )
 select * from total
 where r < 3;
+
+
+
+
+
+with ch as
+(select e.name, d.dept_name, count(ep.project_id) as project_count,
+row_number() over(partition by e.dept_id order by count(ep.project_id) desc) as rrk
+from employees e
+join departments d on e.dept_id = d.dept_id
+join employee_project_assignments ep on ep.emp_id = e.emp_id
+group by name, dept_name, d.dept_id)
+select * 
+from ch
+where rrk < 3;
