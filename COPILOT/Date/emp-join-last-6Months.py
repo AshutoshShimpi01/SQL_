be0abd0f2ad1e199abd062a96a32a4c076e1d756
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-11-27 11:55:39 +0530

    Update emp-join-last-6Months.py

--- COPILOT/Date/emp-join-last-6Months.py
+++ COPILOT/Date/emp-join-last-6Months.py
@@ -3,3 +3,14 @@
 select *
 from employees
 where joining_date >= date_add(current_date(), interval -60 month ); 
+
+
+
+
+
+
+-- employees join in last (1500 days)
+
+select *
+from employees
+where datediff(current_date(), joining_date) <= 1500; 
